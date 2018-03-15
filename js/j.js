e925ecd7f3d08fb9f343373b1b0ee6c8f3127000
LucasNG521 <35329697+LucasNG521@users.noreply.github.com>
2018-03-15 15:21:32 +0800

    Update j.js

--- js/j.js
+++ js/j.js
@@ -1,23 +1,10 @@
-$('a[href="#about"]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
-});
-
-$('a[href="#interest"]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
-});
-
-$('a[href="#contact"]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
-});
-
-$('a[href="#pagetop"]').on('click', function (e) {
-    e.preventDefault();
-    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top }, 500, 'linear');
-});
 
 $('#img1').click(function () {
     $('#slide').slideDown(5000);
+});
+
+
+$('body').on('click','a[href*=#]', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 });
